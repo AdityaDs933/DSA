17f39a3fd41d5cfe198fe11e1eb40ea6ec6d82dc
Aditya SIngh <adityapc933@gmail.com>
2026-09-23 21:33:13 +0530

    Time: 1 ms (95.45%) | Memory: 44.1 MB (25.14%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int sum=candies[0];
+        for(int i=1;i<candies.length;i++){
+            if(candies[i]>sum){
+                sum=candies[i];
+            }
+        }
+        List<Boolean> result=new ArrayList<>();
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]+extraCandies>=sum){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
+        
+    }
+}
